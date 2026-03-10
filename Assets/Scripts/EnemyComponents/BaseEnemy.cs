01480b9b7d2cf1ff02277b55b7cfc2b2feba33b3
Kevin LeBlanc <keleblanc24@gmail.com>
2026-03-10 17:07:11 -0400

    updated base enemy
    Added some basic enemy behavior. :100:

--- Assets/Scripts/EnemyComponents/BaseEnemy.cs
+++ Assets/Scripts/EnemyComponents/BaseEnemy.cs
@@ -1,23 +1,28 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class BaseEnemy : MonoBehaviour, IEnemy
 {
-
     [SerializeField] private Transform attackOrigin;
     [SerializeField] private float attackDistance = 1.3f;
     [SerializeField] private float attackRate = 1f;
     [SerializeField] private float damageAmount = 10f;
+    [SerializeField] private float detectionRange = 10f;
+    [SerializeField] private DamageType damageType;
 
-    private UnityEngine.AI.NavMeshAgent navMeshAgent;
+    private NavMeshAgent navMeshAgent;
+    private GameObject player;
+    private bool isAttacking = false;
+
+    private enum EnemyState { Idle, Chase, Attack }
+    private EnemyState currentState = EnemyState.Idle;
 
     #region IEnemy
 
     public void Attack(GameObject target, DamageType type)
     {
-        if (target == null)
-        {
-            return;
-        }
+        if (target == null) return;
 
         RaycastHit hitInfo;
         if (Physics.Raycast(attackOrigin.position, attackOrigin.forward, out hitInfo, attackDistance, 1 << 3))
@@ -27,36 +32,94 @@ public class BaseEnemy : MonoBehaviour, IEnemy
                 IDamageable damageable = target.GetComponent<IDamageable>();
                 if (damageable != null)
                 {
-                    damageable.ApplyDamage(new DamageRequest(damageAmount, gameObject,
-                        target.transform.position, hitInfo.normal, type));
+                    damageable.ApplyDamage(new DamageRequest(
+                        damageAmount,
+                        gameObject,
+                        target.transform.position,
+                        hitInfo.normal,
+                        type));
                 }
             }
         }
     }
 
+    // Resumes NavMeshAgent movement toward last set destination
     public void Move()
     {
-        throw new System.NotImplementedException();
+        navMeshAgent.isStopped = false;
     }
 
+    // Sets the NavMeshAgent destination to the target's position
     public void MoveTo(GameObject target)
     {
-        navMeshAgent.Move(target.transform.position);
+        if (target == null) return;
+        navMeshAgent.isStopped = false;
+        navMeshAgent.SetDestination(target.transform.position);
     }
 
     #endregion
 
     #region MB
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
-        
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindWithTag("Player");
     }
 
-    // Update is called once per frame
     void Update()
     {
-
+        UpdateState();
+        HandleState();
     }
+
+    #endregion
+
+    #region State Machine
+
+    private void UpdateState()
+    {
+        if (player == null) return;
+
+        float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+
+        if (distanceToPlayer <= attackDistance)
+            currentState = EnemyState.Attack;
+        else if (distanceToPlayer <= detectionRange)
+            currentState = EnemyState.Chase;
+        else
+            currentState = EnemyState.Idle;
+    }
+
+    private void HandleState()
+    {
+        switch (currentState)
+        {
+            case EnemyState.Idle:
+                navMeshAgent.isStopped = true;
+                break;
+
+            case EnemyState.Chase:
+                MoveTo(player);
+                break;
+
+            case EnemyState.Attack:
+                navMeshAgent.isStopped = true;
+                // Face the player before attacking
+                transform.LookAt(player.transform.position);
+                if (!isAttacking)
+                    StartCoroutine(AttackRoutine());
+                break;
+        }
+    }
+
+    private IEnumerator AttackRoutine()
+    {
+        isAttacking = true;
+        Attack(player, damageType);
+        yield return new WaitForSeconds(1f / attackRate);
+        isAttacking = false;
+    }
+
     #endregion
 }
